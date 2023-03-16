829f324b25478ba8b853aa5e1c8e9f87f6ff6280
Riccardo <64649314+Migl992@users.noreply.github.com>
2023-03-16 10:19:50 +0100

    new contract

--- test/test.js
+++ test/test.js
@@ -3,58 +3,63 @@ const {
   loadFixture,
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+const { expect, assert } = require("chai");
 
 describe("BadgerNFT", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function contractIn() {
-
+    // deployOneYearLockFixture
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const BadgerNFT = await hre.ethers.getContractFactory("BadgerNFT");
-    const Badger = await BadgerNFT.deploy();
+    const BadgerNFT = await ethers.getContractFactory("BadgerNFT");
+    const badger = await BadgerNFT.deploy();
 
-    return { Badger, owner, otherAccount };
+    return { badger, owner, otherAccount };
   }
 
   describe("Deployment", function () {
-    it("Deploy ok", async function () {
-      const { Badger } = await loadFixture(contractIn);
+    it("deploys OK", async () => {
+      const { badger } = await loadFixture(contractIn);
+      // const address = await smartContract.address;
+      console.log(badger.address);
+      // expect(contract.address).not.to.equal(0x0)
 
-      console.log(Badger.address);
+      assert.notEqual(badger.address, 0x0);
+      assert.notEqual(badger.address, "");
     });
 
     it("Should set the right owner", async function () {
-      const { Badger , owner} = await loadFixture(contractIn);
+      const { badger, owner } = await loadFixture(contractIn);
 
-      expect(await Badger.owner()).to.equal(owner.address);
+      expect(await badger.owner()).to.equal(owner.address);
     });
   });
+
   describe("Minting", () => {
     it("mints an NFT successfully to the owner", async () => {
-      const { Badger, owner} = await loadFixture(
-        contractIn 
-      );
-
-      await Badger.safeMint(owner.address); // mint an NFT to the owner
-
-      assert.equal(await Badger.balanceOf(owner.address), 1); // check that the account balance of the owner is correct having minted 1 NFT
-    });
-
-    it("mints and transfers NFT successfully from the owner to another account", async () => {
-      const { Badger, owner, otherAccount } = await loadFixture(
+      const { badger, owner } = await loadFixture(
         contractIn
       );
 
-      await Badger.safeMint(owner.address); // mint an NFT to the owner
-      await Badger.safeMint(owner.address); // mint an NFT to the owner
+      await badger.safeMint(owner.address); // mint an NFT to the owner
 
-      await Badger.transferFrom(owner.address, otherAccount.address, 1); // transfer the minted NFT from the owner to the otherAccount
+      assert.equal(await badger.balanceOf(owner.address), 1); // check that the account balance of the owner is correct having minted 1 NFT
+    });
 
-      assert.equal(await Badger.balanceOf(otherAccount.address), 1); // check that the account balance of the otherAccount is correct having received 1 NFT
+    it("mints and transfers NFT from the owner to another account", async () => {
+      const { badger, owner, otherAccount } = await loadFixture(
+        contractIn
+      );
+
+      await badger.safeMint(owner.address); // mint an NFT to the owner
+      await badger.safeMint(owner.address); // mint an NFT to the owner
+
+      await badger.transferFrom(owner.address, otherAccount.address, 1); // transfer the minted NFT from the owner to the otherAccount
+
+      assert.equal(await badger.balanceOf(otherAccount.address), 1); // check that the account balance of the otherAccount is correct having received 1 NFT
 
       // Uncomment these to ensure that both the owner and otherAccount hold 1 NFT
       // console.log(await contract.balanceOf(owner.address));
@@ -62,6 +67,3 @@ describe("BadgerNFT", function () {
     });
   });
 });
-
-
-
